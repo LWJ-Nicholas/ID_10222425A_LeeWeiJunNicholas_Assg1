77d52d9fbe35e2afdf8294400ecdad48cbf808cf
Wei Jun Nicholas Lee <s10222425@connect.np.edu.sg>
2021-11-29 05:03:32 +0800

    Done first 3 pages

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.backgroundColor = "#000";
+    document.getClassById("navbar").style.backgroundColor = 'rgba(0,0,0,0)';
   } else {
-    document.getElementById("navbar").style.backgroundColor = "#fff";
+    document.getClassById("navbar").style.backgroundColor = 'white';
   }
 }
 
